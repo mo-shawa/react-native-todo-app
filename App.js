18944a495bc2e7420e3ff3a01abade5bcff0cad3
Mahmoud El-Shawa <moshawa0@gmail.com>
2022-02-11 04:27:55 -0500

    basic unstyled todo list

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [todoItem, setTodoItem] = useState('')
+  const [todoList, setTodoList] = useState([])
+  const addTodoItem = () => {
+    setTodoList([...todoList, todoItem])
+    setTodoItem('')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput style={styles.textInput} placeholder='Enter Todo' onChangeText={text => setTodoItem(text)} value={todoItem} />
+        <Button title='Add Todo' onPress={() => addTodoItem()} />
+      </View>
+      <View>
+        <Text>List of Todos</Text>
+        {todoList.map((todo) => {
+          return <Text>{todo}</Text>
+        })}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 60,
   },
+  textInput: {
+    padding: 10,
+    borderColor: 'blue',
+    borderWidth: 1,
+    marginBottom: 10
+  }
 });
