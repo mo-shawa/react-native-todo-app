528d8f2244932b645c61d904be8fcdb8544a9e43
Mahmoud El-Shawa <moshawa0@gmail.com>
2022-02-11 11:18:52 -0500

    move todo input to bottom, fix adding empty todos

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
   const [todoItem, setTodoItem] = useState('')
   const [todoList, setTodoList] = useState([])
   const addTodoItem = () => {
+    if (todoItem.trim() == '') return
     setTodoList([...todoList, todoItem])
     setTodoItem('')
   }
@@ -16,10 +17,17 @@ export default function App() {
         <Button title='Add Todo' onPress={() => addTodoItem()} />
       </View>
       <View>
-        <Text>List of Todos</Text>
-        {todoList.map((todo) => {
-          return <Text>{todo}</Text>
-        })}
+        <Text style={styles.heading}>List of Todos</Text>
+
+        <ScrollView>
+          {todoList.map((todo, index) => {
+            return (
+              <View key={index} style={styles.todoItem}>
+                <Text>{todo}</Text>
+              </View>
+            )
+          })}
+        </ScrollView>
       </View>
     </View>
   );
@@ -28,11 +36,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     padding: 60,
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    justifyContent: 'space-between',
+    height: '100%'
   },
   textInput: {
     padding: 10,
     borderColor: 'blue',
-    borderWidth: 1,
+    borderWidth: 0.5,
+    borderRadius: 3,
     marginBottom: 10
+  },
+  heading: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginVertical: 10
+  },
+  todoItem: {
+    borderBottomColor: '#222222',
+    borderBottomWidth: 1,
+    marginVertical: 5,
+    padding: 15
   }
 });
